02a49400085af9812b37ca33aeab7efa37bc9147
Crystal <chi200706@gmail.com>
2024-08-10 16:26:10 +0800

    SSL是否啟用改吃.env

--- config/admin.php
+++ config/admin.php
@@ -114,7 +114,7 @@ return [
     | If your page is going to be accessed via https, set it to `true`.
     |
     */
-    'https' => 'true',
+    'https' => env('ADMIN_HTTPS', false),
 
     /*
     |--------------------------------------------------------------------------
