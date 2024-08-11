18bb9acc521c4fe78bcf40c31e9099a0cb099c6e
Crystal <chi200706@gmail.com>
2024-08-11 22:46:28 +0800

    更換網站三處icon

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<img src="/vendor/dcat-admin/images/logo.png" width="35"> &nbsp;生活後台',
+    'logo' => '<img src="/vendor/dcat-admin/images/cupcake.png" width="35"> &nbsp;生活後台',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<img src="/vendor/dcat-admin/images/logo.png">',
+    'logo-mini' => '<img src="/vendor/dcat-admin/images/cupcake.png">',
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'favicon' => null,
+    'favicon' => '/vendor/dcat-admin/images/cupcake.png',
 
     /*
      |--------------------------------------------------------------------------
