4623e99b263cae9cda0d29068cd1acd566c960c5
Crystal <chi200706@gmail.com>
2024-09-14 20:49:59 +0800

    增加營業時段欄位

--- app/Admin/Controllers/RestaurantController.php
+++ app/Admin/Controllers/RestaurantController.php
@@ -50,16 +50,31 @@ class RestaurantController extends AdminController
                 })->toArray();
                 $selector->select('price_level', '價位', $price_level);
 
+                // varchar型態的字串只好自定義查詢
+                $business_hours_result = Restaurant::all()->pluck('business_hours')->toArray(); // 避免選單選項哪天改了，還是從餐廳抓取全部存在的時段..
+                $business_hours = [];
+                foreach ($business_hours_result as $business_hour) {
+                    if ($business_hour != null) {
+                        $business_hours = array_merge($business_hours, json_decode($business_hour, true));
+                    }
+                }
+                $unique_business_hours = array_unique($business_hours);
+                $selector->select('business_hours', '營業時段', $unique_business_hours, function ($query, $value_from_page) use ($unique_business_hours) { // 那個$value_from_page竟然是string..
+                    $query->where('business_hours','like', '%'.$unique_business_hours[(int)$value_from_page].'%'); // like效能很差欸...誰叫你要在varchar存json字串...
+                });
+
                 $selector->select('style', '風格', Restaurant::all()->pluck('style', 'style')->toArray());
 
                 $element_result = Restaurant::all()->pluck('element')->toArray();
                 $elements = [];
                 foreach ($element_result as $element) {
-                    $elements = array_merge($elements, json_decode($element, true));
+                    if ($element != null) {
+                        $elements = array_merge($elements, json_decode($element, true));
+                    }
                 }
-                $uniqueElements = array_unique($elements);
-                $selector->select('element', '主要品項', $uniqueElements, function ($query, $value_from_page) use ($uniqueElements) { // 自定義查詢；那個$value_from_page竟然是string..
-                    $query->where('element','like', '%'.$uniqueElements[(int)$value_from_page].'%'); // like效能很差欸...誰叫你要在varchar存json字串...
+                $unique_elements = array_unique($elements);
+                $selector->select('element', '主要品項', $unique_elements, function ($query, $value_from_page) use ($unique_elements) { 
+                    $query->where('element','like', '%'.$unique_elements[(int)$value_from_page].'%'); 
                 });
             });
 
@@ -76,7 +91,13 @@ class RestaurantController extends AdminController
                 5 => '#7357ab',
             ])->sortable();
 
+            
+            $grid->column('business_hours', '營業時段')->display(function ($business_hours) {
+                return json_decode($business_hours, true); // 字串型別的json還原回php array
+            })->label();
+
             $grid->column('style', '風格');
+
 
             $grid->column('element', '主要品項')->display(function ($element) {
                 return json_decode($element, true); // 字串型別的json還原回php array
@@ -121,6 +142,9 @@ class RestaurantController extends AdminController
             });
             $form->text('name', '名稱');
             $form->text('zone', '地區');
+            $form->checkbox('business_hours', '營業時段')->options(['早餐' => '早餐', '午餐' => '午餐','午晚之間空班' => '午晚之間空班', '晚餐(比較早關)' => '晚餐(比較早關)', '晚餐(比較晚關)' => '晚餐(比較晚關)', '宵夜' => '宵夜'])->saving(function ($value) {
+                return json_encode($value, JSON_UNESCAPED_UNICODE);
+            });
             $form->select('price_level', '價位')->options([1 =>'★', 2 => '★★', 3 => '★★★', 4 => '★★★★', 5 => '★★★★★']);
             $form->text('style', '風格');
             $form->tags('element', '主要品項')->placeholder('請用逗號分隔')->saving(function ($value) {
